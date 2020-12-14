d59ef27c255022dcb8c419b54c8e38286bf4fc73
maxim.ovsyannikov <ovsyannikov.m.vik@sberbank.ru>
2020-12-14 10:01:10 +0300

    login page

---
+++ src/Main.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { store } from './auth'
+  function logout() {
+    $store.user = null
+  }
+</script>
+<main>
+  <h2>You are logged in <button type="button" on:click={logout}>Log Out</button></h2>
+  <pre>
+    {JSON.stringify($store, null, 2)}
+  </pre>
+</main>
