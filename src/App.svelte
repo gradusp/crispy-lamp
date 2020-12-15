0ca77c156b4463f53be03eb95cefba3e24ae07a0
MaxOvsyan <37299332+MaxOvsyan@users.noreply.github.com>
2020-12-15 11:03:36 +0300

    Login admin admin (#4)
    * added clean svelte+ts
    
    * Added PostCSS+TailwindCSS v2
    
    * enabled spa flag
    
    * updated vscode extensions recomendations
    
    * login page
    
    * refactoring
    
    Co-authored-by: Pavel Fiskovich <pavel.fiskovich@gmail.com>
    Co-authored-by: maxim.ovsyannikov <ovsyannikov.m.vik@sberbank.ru>

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
-<script lang="ts">
-	export let name: string;
+<script>
+	import Login from './Login.svelte'
+	import Main from './Main.svelte'
+	import { store } from './auth'
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#if $store.user !== null}
+		<Main />
+	{:else}
+		<Login />
+	{/if}
 </main>
 
 <style global lang="postcss">
